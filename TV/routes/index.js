ab748fc074cfd55d399b768dc7da705d2f055bf8
acuevas <apac.alexcuevas@gmail.com>
2019-05-06 14:43:36 +0200

    Corrected index.js

--- TV/routes/index.js
+++ TV/routes/index.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express();
-const CompositionController = require('./core/CompositionController');
-const ModuleController = require('./core/ModuleController');
-const ResourceController = require('./core/ResourceController');
 const TvController = require('./core/TvController');
 
 router.get('/', (req, res) => {
@@ -14,40 +11,6 @@ router.get('/', (req, res) => {
 // ------ Object controller ------
 
 //const objectController = require('./core/objectController');
-
-router
-    .route('/compositions/')
-    .post(CompositionController.create)
-    .get(CompositionController.getAll);
-
-router
-    .route('/compositions/:id')
-    .put(CompositionController.update)
-    .delete(CompositionController.remove)
-    .get(CompositionController.getById);
-
-router
-    .route('/modules/')
-    .post(ModuleController.create)
-    .get(ModuleController.getAll);
-
-router
-    .route('/modules/:id')
-    .put(ModuleController.update)
-    .delete(ModuleController.remove)
-    .get(ModuleController.getById);
-
-router
-    .route('/resources/')
-    .post(ResourceController.create)
-    .get(ResourceController.getAll);
-
-router
-    .route('/resources/:id')
-    .put(ResourceController.update)
-    .delete(ResourceController.remove)
-    .get(ResourceController.getById);
-
 router
     .route('/TVs/')
     .post(TvController.create)
