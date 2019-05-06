510b74fac141047540a913a304d135ae931963d1
ben sureau <sureau117@gmail.com>
2019-05-06 12:58:37 +0200

    feat(compositionFrame | tvFrame): add button + and - on frames

--- src/components/compositionFrame.vue
+++ src/components/compositionFrame.vue
@@ -14,8 +14,13 @@
             </v-card-title>
 
             <v-card-actions>
-                <v-btn flat color="orange">Share</v-btn>
-                <v-btn flat color="orange">Explore</v-btn>
+                <v-btn fab dark small>
+                    <v-icon dark>remove</v-icon>
+                </v-btn>
+
+                <v-btn fab dark small>
+                    <v-icon dark>add</v-icon>
+                </v-btn>
             </v-card-actions>
         </v-card>
 <br/>
--- src/components/tvFrame.vue
+++ src/components/tvFrame.vue
@@ -13,9 +13,14 @@
                 </div>
             </v-card-title>
 
-            <v-card-actions>
-                <v-btn flat color="orange">Share</v-btn>
-                <v-btn flat color="orange">Explore</v-btn>
+            <v-card-actions lg-right>
+                <v-btn fab dark small>
+                    <v-icon dark>remove</v-icon>
+                </v-btn>
+
+                <v-btn fab dark small>
+                    <v-icon dark>add</v-icon>
+                </v-btn>
             </v-card-actions>
         </v-card>
 
