fd782b68da84140244e3b0ddab995295220d1533
theymard <thomas.eymard@orange.fr>
2019-05-14 10:51:08 +0200

    add second screen

---
+++ src/components/compositionEditFrame.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <menu-bar></menu-bar>
+        <v-app id="inspire">
+            <v-layout row wrap>
+                test
+                sdẑdmdpzmdzpdzpdzd
+            </v-layout>
+        </v-app>
+    </div>
+</template>
+
+<script>
+    import MenuBar from './menuBar.vue'
+
+    export default {
+        name: "EditComposition",
+
+        components : {
+            'menuBar': MenuBar,
+        },methods: {
+            orderList() {
+                this.list = this.list.sort((one, two) => {
+                    return one.order - two.order;
+                });
+            },
+            onMove({ relatedContext, draggedContext }) {
+                const relatedElement = relatedContext.element;
+                const draggedElement = draggedContext.element;
+                console.log("okokokokokoko");
+                return (
+                    (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+                );
+            }
+        },
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,24 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import MainFrame from '@/components/mainFrame';
+import EditComposition from '@/components/compositionEditFrame';
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Router);
 
 export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
 	routes: [
 		{
 			path: '/',
 			name: 'MainFrame',
 			component: MainFrame
+		},
+		{
+			path: '/EditComposition',
+			name: 'EditComposition',
+			component: EditComposition
 		}
 	]
 });
